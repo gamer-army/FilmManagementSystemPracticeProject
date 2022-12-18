c8c8126e9d9e8d299eda2185bc32d12ef21357c7
Daniel James DOLOR <danieljames.dolor@amadeus.com>
2022-12-18 10:04:00 +0800

    adding more film tests; also nakakapagud naman mag TestDrivenDevelopement. So many test case for such a small class file

--- tests/FilmManagementSystem.Tests.Core/FilmTests.cs
+++ tests/FilmManagementSystem.Tests.Core/FilmTests.cs
@@ -5,6 +5,7 @@ public class FilmTests
     public class FilmID
     {
         const string InvalidIDMessage = "id should be alphanumeric of 4 chars (Parameter 'id')";
+
         [Theory]
         [InlineData("10")]
         [InlineData("4AAA2")]
@@ -24,6 +25,31 @@ public class FilmTests
         public void ShouldAcceptValidID(string ID)
         {
             Action act = () => new Film(ID, "Valid Title", new TimeSpan(1,30,0), 0);
+            var error = Record.Exception(() => act.Invoke());
+            Assert.Null(error);
+        }
+    }
+
+    public class FilmTitle
+    {
+        const string InvalidTitleMessage = "title should be 3-100 characters long (Parameter 'title')";
+
+        [Theory]
+        [InlineData("AM")]
+        [InlineData("AMongus Saurus Sexy Benis With Big Smelly Balls What the fuck am i doing with my life seriously God left me unfinished")]
+        public void ShouldThrowExceptionForInvalidTitle(string title)
+        {
+            Action act = () => new Film("45AA", title, new TimeSpan(1,30,0), 0);
+            var error = Assert.Throws<ArgumentException>(act);
+            Assert.Equal(InvalidTitleMessage, error.Message);
+        }
+        
+        [Theory]
+        [InlineData("Amongus")]
+        [InlineData("Kanye West is not the enemy")]
+        public void ShouldAcceptValidTitle(string title)
+        {
+            Action act = () => new Film("45AA", title, new TimeSpan(1,30,0), 0);
             var error = Record.Exception(() => act.Invoke());
             Assert.Null(error);
         }
