c9b3b2ba00e8363ddff3b6a456ee7599742045cd
Daniel James DOLOR <danieljames.dolor@amadeus.com>
2022-12-18 18:51:09 +0800

    adding customer and customertests

--- src/FilmManagementSystem.Core/ExtensionHelperMethods.cs
+++ src/FilmManagementSystem.Core/ExtensionHelperMethods.cs
@@ -6,4 +6,15 @@ public static class ExtensionHelperMethods
     {
         return Regex.IsMatch(val, "^[A-Za-z0-9]{1,}$");
     }
+
+    public static bool IsValidName(this string val)
+    {
+        return (val.Length > 2 && val.Length < 101) && 
+            Regex.IsMatch(val, "^[A-Z][A-Za-z]{1,}( [A-Z][A-Za-z]{1,}){0,}$");
+    }
+
+    public static bool IsPast(this DateOnly date)
+    {
+        return date < DateOnly.FromDateTime(DateTime.Now);
+    }
 }
---
+++ tests/FilmManagementSystem.Tests.Core/CustomerTests.cs
@@ -0,0 +1,16 @@
+namespace FilmManagementSystem.Tests.Core;
+
+public class CustomerTests
+{
+    [Theory]
+    [InlineData(2000,10,5)]
+    [InlineData(1991,1,10)]
+    public void ShouldAcceptPastDatedDateOfBirth(int year, int month, int day)
+    {
+        var DateOfBirth = new DateOnly(year, month, day);
+        Action act = () => new Customer("Bobby", "McDimsdale", DateOfBirth);
+        
+        var error = Record.Exception(act);
+        Assert.Null(error);
+    }
+}
